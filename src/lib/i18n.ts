889f9cdf2acb20af0f95fa23b208dd5bfb276c23
Torrenté Florian <lukyrouge4@gmail.com>
2025-02-02 23:13:49 +0100

    fix: update project and invite tables to use identity columns and clean up i18n initialization

--- src/lib/i18n.ts
+++ src/lib/i18n.ts
@@ -1,20 +1,22 @@
 import i18next from 'i18next';
 import { createI18nStore } from 'svelte-i18next';
 import Backend from 'i18next-http-backend';
-import { i18nextPlugin } from 'translation-check'
+import { i18nextPlugin } from 'translation-check';
 
 // Load translations from json
 
-i18next.use(Backend).use(i18nextPlugin).init({
-	lng: 'en',
-	backend: {
-		loadPath: '/locales/{{lng}}.json'
-	},
-	interpolation: {
-		escapeValue: false // not needed for svelte as it escapes by default
-	}
-});
+i18next
+	.use(Backend)
+	// .use(i18nextPlugin)
+	.init({
+		lng: 'en',
+		backend: {
+			loadPath: '/locales/{{lng}}.json'
+		},
+		interpolation: {
+			escapeValue: false // not needed for svelte as it escapes by default
+		}
+	});
 
 const i18n = createI18nStore(i18next);
-console.log(i18n);
 export default i18n;
