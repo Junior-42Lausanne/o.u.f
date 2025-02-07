e1a049b00ffd73cc247af55bf600a86d69cbcb6e
Torrenté Florian <lukyrouge4@gmail.com>
2025-02-07 19:39:08 +0100

    feat: front - start copying the navbar

---
+++ src/routes/(front)/+layout.svelte
@@ -0,0 +1,44 @@
+<header class="top-0 z-50 w-full bg-white text-center shadow-[0_0_20px_0_rgba(0,0,0,0.20)]">
+	<nav class="flex min-h-[52px] flex-row items-center justify-center">
+		<a class="absolute left-[15px] flex items-center opacity-30" href="/">
+			<img src="/logo.svg" alt="Unlimited cities logo" />
+			<span class="mx-[5px] inline-block bg-black p-[2px] italic text-white">BETA</span>
+		</a>
+		<ul>
+			<li class="inline-block justify-center">
+				<a
+					href="/mix"
+					class="inline-block w-36 justify-center border-b-4 border-gray-800 py-3 text-center text-2xl font-bold text-gray-800"
+				>
+					Mix
+				</a>
+			</li>
+			<li class="inline-block justify-center">
+				<a
+					href="/mix"
+					class="inline-block w-36 justify-center border-gray-800 py-3 text-center text-2xl font-extralight text-gray-800"
+				>
+					Carte
+				</a>
+			</li>
+			<li class="inline-block justify-center">
+				<a
+					href="/mix"
+					class="inline-block w-36 justify-center border-gray-800 py-3 text-center text-2xl font-extralight text-gray-800"
+				>
+					Galerie
+				</a>
+			</li>
+		</ul>
+		<div class="absolute right-0 top-[10px] inline-block pb-[5px] text-left text-gray-800"></div>
+	</nav>
+</header>
+
+<svelte:head>
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
+	<link
+		href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap"
+		rel="stylesheet"
+	/>
+</svelte:head>
