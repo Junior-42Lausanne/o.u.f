52fbb166412fea856c3411643dbb0e7e3218b95a
Torrenté Florian <lukyrouge4@gmail.com>
2025-02-18 18:25:36 +0100

    feat: add language selector and flags for English and French

--- src/routes/(front)/+layout.svelte
+++ src/routes/(front)/+layout.svelte
@@ -1,8 +1,28 @@
 <script lang="ts">
 	import { page } from '$app/state';
 	let { children } = $props();
+	import i18n from '$lib/i18n';
+	import { languages } from '$lib/translation';
+	import { onMount } from 'svelte';
 
 	const pathname = $derived(page.url.pathname);
+	let lang_selector_open = $state(false);
+
+	function handleClickOutside(event: MouseEvent) {
+		// if target id is not lang_selector, close lang_selector
+		if (!(event.target as HTMLElement).closest('#lang_selector')) {
+			lang_selector_open = false;
+		}
+	}
+
+	onMount(() => {
+		document.addEventListener('click', handleClickOutside);
+		return () => {
+			document.removeEventListener('click', handleClickOutside);
+		};
+	});
+
+	const current_lang = $derived($i18n.resolvedLanguage);
 </script>
 
 <header
@@ -52,12 +72,40 @@
 			</li>
 		</ul>
 		<div class="absolute right-0 text-left text-gray-800">
-			<div class="cursor-pointer px-2 text-base">
-				<span class="">
-					<img src="/flags/es.svg" alt="ES flag" class="inline h-4" />
-					<span>Espagnol ▾</span>
-					<span></span>
-				</span>
+			{#if current_lang}
+				<button
+					class="cursor-pointer px-2 text-base"
+					id="lang_selector"
+					onclick={() => (lang_selector_open = !lang_selector_open)}
+				>
+					<span class="">
+						<img
+							src="/flags/{$i18n.language}.svg"
+							alt={current_lang.toUpperCase()}
+							class="inline h-4"
+						/>
+						<span>{$i18n.t(`global.lang.${current_lang}`)} ▾</span>
+					</span>
+				</button>
+			{/if}
+			<div
+				class="absolute z-[999] mt-2 max-h-[160px] overflow-auto rounded border border-gray-300 bg-white py-2"
+				hidden={!lang_selector_open}
+			>
+				{#each languages as lang}
+					<button
+						onclick={() => {
+							$i18n.changeLanguage(lang);
+							lang_selector_open = false;
+						}}
+						class="my-1 cursor-pointer whitespace-nowrap pb-[3px] pl-2 pr-[20px] hover:bg-gray-200"
+					>
+						<span class="">
+							<img src="/flags/{lang}.svg" alt={lang.toUpperCase()} class="inline h-4" />
+							<span>{$i18n.t(`global.lang.${lang}`)}</span>
+						</span>
+					</button>
+				{/each}
 			</div>
 		</div>
 	</nav>
